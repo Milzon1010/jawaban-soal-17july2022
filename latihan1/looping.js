4649f659b4d89e9a2bea499e047ab07505a0a530
Milzon1010 <106896186+Milzon1010@users.noreply.github.com>
2022-07-23 02:25:57 +0700

    jawaban

---
+++ latihan1/looping.js
@@ -0,0 +1,15 @@
+/**
+ * SOAL 1
+ * 
+ * Faktor merupakan deretan angka yang membentuk suatu angka tertentu.
+ * Contoh: Faktor dari 10 adalah angka 1, 2, 5 dan 10.
+ * Ciri-ciri faktor adalah angka tersebut habis membagi.
+     var number = 10;
+    // Results
+    // 1,2,5,10
+ * 
+ * Buat sebuah program untuk menghasilkan faktor dari sebuah angka.
+ * 
+ * */ 
+
+var number = 10;
