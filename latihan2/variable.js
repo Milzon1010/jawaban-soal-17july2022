7b52d668df5e7a268f5eea78f2085e14092ca99b
Milzon1010 <106896186+Milzon1010@users.noreply.github.com>
2022-07-23 02:28:23 +0700

    jawaban

---
+++ latihan2/variable.js
@@ -0,0 +1,56 @@
+/**
+ * SOAL 1
+ *
+ * Menghitung volume setengah bola dengan rumus:
+ *
+ * volume = 1/2 x phi x r^3
+ *
+ * Tentukan algoritma, pseudocode, dan codingan untuk soal dia atas
+ * */
+
+// var phi = 3.14
+// var r = 12
+
+// let volume (1/2 * phi x r**3);
+// console.log(volume);
+
+/**
+ * SOAL 2
+ *
+ * Menampilkan hasil seperti berikut:
+ *
+ * Result
+ *
+ * Selamat belajar, Vincent!
+ *
+ * */
+
+// let word ="Selamat belajar, Vincent!"
+// console.log(word);
+
+/**
+ * SOAL 3
+ *  
+ * Terdapat 3 bagian: <nama_angkarandom_boolean>
+ * 
+ * Result
+ * 
+    * // 1st running
+        admin1true
+    // 2nd running
+        admin5true
+ * 
+ * */
+
+var name = "admin";
+var status = true;
+// random dari angka 1-9
+var number;
+
+let result = '';
+if (result) {
+    status = true;
+    } 
+
+
+
